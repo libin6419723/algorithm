14c029b94dd31070f2b6971d16c60e5869acc3a2
root <root@pve.libin.home>
2019-11-19 15:34:56 +0800

    starting

---
+++ 1.1/js/test.js
@@ -0,0 +1,7 @@
+
+var fs = require("fs")
+
+var data =  fs.readFileSync("../data.txt");
+console.log(data.toString());
+function sort() {
+}
---
+++ 1.1/test.py
@@ -0,0 +1,4 @@
+import sys, random
+
+for i in range(0, 1000):
+    print random.randint(100,99999)
